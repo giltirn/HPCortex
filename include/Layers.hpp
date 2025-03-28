4ec874b4de4ba0d7b2178fea614d43555d13ff7a
Christopher Kelly <ckelly@bnl.gov>
2025-03-28 14:42:59 -0400

    Cleaned up Layers header by moving function bodies to implementation file

--- include/Layers.hpp
+++ include/Layers.hpp
@@ -82,196 +82,22 @@ public:
   DNNlayer(DNNlayer &&r) = default;
   
   //Forward pass
-  Matrix<FloatType> value(const Matrix<FloatType> &x){
-    ++calls;
-    
-    Matrix<FloatType> in = leaf.v.value(x);
-    batch_size = x.size(1);   
-    assert(in.size(0) == size1);
-    assert(in.size(1) == batch_size);
-   
-    Matrix<FloatType> out(size0,batch_size);
-    {
-      autoView(bias_v,bias,DeviceRead);
-      autoView(out_v,out,DeviceWrite);
-      autoView(in_v,in,DeviceRead);
-      autoView(weights_v,weights,DeviceRead);
+  Matrix<FloatType> value(const Matrix<FloatType> &x);
 
-      //Basic version where columns are summed over within a thread and rows/batches distributed over threads
-      size_t sz1 = size1;
-      accelerator_for2d(b,batch_size,i,size0,1,{
-	  FloatType v = bias_v(i);
-	  for(int j=0;j<sz1;j++)
-	    v += weights_v(i,j)* in_v(j,b);
-	  out_v(i,b) = v;	  
-	});      
-    }
+  void deriv(Vector<FloatType> &cost_deriv, int off, Matrix<FloatType> &&_above_deriv, Matrix<FloatType>* input_above_deriv_return = nullptr) const;
 
-    //Apply activation function ; modifies output in-place and returns derivatives   
-    Matrix<FloatType> activation_deriv;
-    activation_func(out, &activation_deriv);
-    assert(activation_deriv.size(0) == size0);
-    assert(activation_deriv.size(1) == batch_size);
-
-    leaf_buf.push(std::move(in));
-    activation_deriv_buf.push(std::move(activation_deriv));
-    
-    return out;
-  }
-
-  void deriv(Vector<FloatType> &cost_deriv, int off, Matrix<FloatType> &&_above_deriv, Matrix<FloatType>* input_above_deriv_return = nullptr) const{
-    profileStart();
-    assert(_above_deriv.size(0) == size0);
-    assert(_above_deriv.size(1) == batch_size);
-    int p=off;
-    Matrix<FloatType> layer_deriv;
-    {
-      Matrix<FloatType> above_deriv(std::move(_above_deriv)); //inside the braces above ensures this object is freed before the next layer is called
-      
-      //until the pipeline is "primed", the ring buffers will pop uninitialized values. We could in principle skip doing any computation until then
-      //but for now we just initialize with zero values (TODO: revisit)
-      Matrix<FloatType> in = leaf_buf.isFilled() ? leaf_buf.pop(): Matrix<FloatType>(size1,batch_size,0.);
-      assert(in.size(0) == size1);
-      assert(in.size(1) == batch_size);
-      
-      Matrix<FloatType> activation_deriv = activation_deriv_buf.isFilled() ? activation_deriv_buf.pop() : Matrix<FloatType>(size0,batch_size,0.);
-      assert(activation_deriv.size(0) == size0);
-      assert(activation_deriv.size(1) == batch_size);      
-
-      //for reverse differentiation, we pass down the derivatives of the cost with respect to our inputs, x (vector)
-      //Write output  f_i(x) = A( g_i(x) )   where A is the activation function
-      //where g_i(x) = b_i + \sum_j w_ij x_j
-      //
-      //dcost / dx_j = \sum_i dcost/df_i df_i/dx_j
-      //df_i/dx_j = df_i / dg_i dg_i / dx_j
-      //dg_i/dx_j = w_ij
-      
-      //dcost / dx_j = \sum_i (dcost/df_i df_i/dg_i) w_ij     :  "layer deriv"
-      
-      //precompute the "activated derivatives"  (dcost/df_i df_i/dg_i) as they are reused below
-
-      labelRegionBegin("precompute_activated_derivs");
-      Matrix<FloatType> activated_above_deriv = computeThinMatOuterProd(above_deriv,activation_deriv);
-      labelRegionEnd();
-      
-      //Compute layer deriv
-      labelRegionBegin("compute_layer_deriv");
-      layer_deriv = mulMatTransposeThinMat(weights, activated_above_deriv);
-      labelRegionEnd();
-
-      //Now we finish up the derivs wrt our parameters      
-      //dcost / dw_jk = \sum_i (dcost/df_i df_i/dg_i) dg_i/dw_jk
-      //dcost / db_j = \sum_i (dcost/df_i df_i/dg_i) dg_i/db_j
-      
-      //dg_i / d w_jk = delta_ij x_k
-      //dg_i / d b_j = delta_ij
-      
-      {
-	labelRegionBegin("cost_deriv_weights");
-	size_t bs = batch_size;
-	
-	//this version saves the overheads of the intermediate copy
-	autoView(cost_deriv_v,cost_deriv,DeviceReadWrite);
-	thinMulMatMatTranspose_p(cost_deriv_v.data() + p, activated_above_deriv, in);     
-	
-	p += size0*size1;
-	labelRegionEnd();
-	labelRegionBegin("cost_deriv_bias");
-	autoView(activated_above_deriv_v,activated_above_deriv,DeviceRead);
-	accelerator_for2d(dummy1,1, j,size0, 64,{
-	    int pp = p + j;
-	    FloatType v = activated_above_deriv_v(j,0);
-	    for(int b=1;b<bs;b++)
-	      v += activated_above_deriv_v(j,b);
-	    cost_deriv_v(pp) = v;
-	  });
-
-	p += size0;
-
-	labelRegionEnd();
-      }
-    
-    }//close views and free temporaries before calling layer below
-    
-    leaf.v.deriv(cost_deriv, p, std::move(layer_deriv), input_above_deriv_return);
-    profileStop();
-  }
-
-  void update(int off, const Vector<FloatType> &new_params){
-    int p=off;
-    {
-      autoView(new_params_v,new_params,DeviceRead);
-      autoView(bias_v,bias,DeviceWrite);
-      autoView(weights_v,weights,DeviceWrite);
-      size_t sz1=size1;
-      accelerator_for2d(j,size1,i,size0,1,{
-	  int pp = p + j + sz1*i;
-	  weights_v(i,j) = new_params_v(pp);
-	});
-	  
-      p += size0*size1;
-
-      accelerator_for(i,size0,{
-	bias_v(i) = new_params_v(p + i);
-	});
-      
-      p += size0;
-    }
-    leaf.v.update(p, new_params);
-  }
-  void step(int off, const Vector<FloatType> &derivs, FloatType eps){
-    int p=off;
-    {
-      autoView(derivs_v,derivs,DeviceRead);
-      autoView(bias_v,bias,DeviceReadWrite);
-      autoView(weights_v,weights,DeviceReadWrite);
-      size_t sz1 = size1;
-      accelerator_for2d(j,size1,i,size0,1,{
-	  int pp = p + j + sz1*i;
-	  weights_v(i,j) -= derivs_v(pp) * eps;
-	});
-	  
-      p += size0*size1;
-
-      accelerator_for(i,size0,{
-	bias_v(i) -= derivs_v(p + i) * eps;
-	});
-      
-      p += size0;
-    }
-    leaf.v.step(p, derivs, eps);
-  }
+  void update(int off, const Vector<FloatType> &new_params);
+  
+  void step(int off, const Vector<FloatType> &derivs, FloatType eps);
 
   //accumulated #params for layers here and below
   inline int nparams() const{ return size0*size1 + size0 + leaf.v.nparams(); }
 
   //off measured from *end*, return new off
-  void getParams(Vector<FloatType> &into, int off){
-    int p = off;
-    {
-      autoView(into_v,into,DeviceReadWrite);
-      autoView(bias_v,bias,DeviceRead);
-      autoView(weights_v,weights,DeviceRead);
-      size_t sz1 = size1;
-      accelerator_for2d(j,size1,i,size0,1,{
-	  int pp = p + j + sz1*i;
-	  into_v(pp) = weights_v(i,j);
-	});
-
-      p += size0*size1;
-	  
-      accelerator_for(i,size0,{
-	into_v(p + i) = bias_v(i);
-	});
-
-      p += size0;
-    }
-    leaf.v.getParams(into, p);
-  }
+  void getParams(Vector<FloatType> &into, int off);
 
   //For pipelining
   inline void resizeInputBuffer(size_t to){
-    //std::cout << "RANK " << rank << " " << this << " RESIZING RING BUFFERS TO " << to << std::endl;
     pipeline_mode = true;
     leaf_buf.resize(to);
     activation_deriv_buf.resize(to);
@@ -308,79 +134,19 @@ public:
   skipConnection(skipConnection &&r) = default;
   
   //Forward pass
-  Matrix<FloatType> value(const Matrix<FloatType> &x){
-    Matrix<FloatType> in = leaf_below.v.value(x);
-    Matrix<FloatType> out = in + leaf_internal.v.value(in);
-    
-    in_buf.push(std::move(in));
-    in_size = in.size(0);
-    batch_size = in.size(1);
+  Matrix<FloatType> value(const Matrix<FloatType> &x);
 
-    return out;
-  }
-
-  void deriv(Vector<FloatType> &cost_deriv, int off, Matrix<FloatType> &&_above_deriv, Matrix<FloatType>* input_above_deriv_return = nullptr) const{
-    assert(_above_deriv.size(0) == in_size);
-    assert(_above_deriv.size(1) == batch_size);
-    int p=off;
-    Matrix<FloatType> layer_deriv;
-    {
-      Matrix<FloatType> above_deriv(std::move(_above_deriv)); //inside the braces above ensures this object is freed before the next layer is called
-      
-      //until the pipeline is "primed", the ring buffers will pop uninitialized values. We could in principle skip doing any computation until then
-      //but for now we just initialize with zero values (TODO: revisit)
-      Matrix<FloatType> in = in_buf.isFilled() ? in_buf.pop(): Matrix<FloatType>(in_size,batch_size,0.);
-      assert(in.size(0) == in_size);
-      assert(in.size(1) == batch_size);
-      
-      //f_i(x) = g_i(x) + x_i
-
-      //deriv wrt inputs for backprop
-      //df_i/dx_j = dg_i/dx_j + delta_ij
-      
-      //dcost / dx_j = \sum_i dcost/df_i df_i/dx_j
-      //             = \sum_i dcost/df_i dg_i/dx_j  + \sum_i dcost/df_i delta_ij
-      //             = \sum_i dcost/df_i dg_i/dx_j  + \sum_i dcost/df_j
-      
-      //deriv wrt params for filling cost_deriv
-      //df_i/dparam_p = dg_i/dparam_p
-
-      layer_deriv = above_deriv; //dcost/df_j
-      Matrix<FloatType> leaf_internal_deriv; //\sum_i dcost/df_i dg_i/dx_j
-      leaf_internal.v.deriv(cost_deriv, p, std::move(above_deriv), &leaf_internal_deriv);
-
-      layer_deriv += leaf_internal_deriv;
-
-      p += leaf_internal.v.nparams();  
-    }//close views and free temporaries before calling layer below
-    
-    leaf_below.v.deriv(cost_deriv, p, std::move(layer_deriv), input_above_deriv_return);
-  }
-
-  void update(int off, const Vector<FloatType> &new_params){
-    int p=off;
-    leaf_internal.v.update(p, new_params);
-    p += leaf_internal.v.nparams();
-    leaf_below.v.update(p, new_params);
-  }
-  void step(int off, const Vector<FloatType> &derivs, FloatType eps){
-    int p=off;
-    leaf_internal.v.step(p, derivs, eps);
-    p += leaf_internal.v.nparams();
-    leaf_below.v.step(p, derivs, eps);
-  }
-
+  void deriv(Vector<FloatType> &cost_deriv, int off, Matrix<FloatType> &&_above_deriv, Matrix<FloatType>* input_above_deriv_return = nullptr) const;
+  
+  void update(int off, const Vector<FloatType> &new_params);
+  
+  void step(int off, const Vector<FloatType> &derivs, FloatType eps);
 
   //accumulated #params for layers here and below
   inline int nparams() const{ return leaf_internal.v.nparams() + leaf_below.v.nparams(); }
 
   //off measured from *end*, return new off
-  void getParams(Vector<FloatType> &into, int off){
-    int p = off;
-    leaf_internal.v.getParams(into, p);
-    p += leaf_internal.v.nparams();
-    leaf_below.v.getParams(into,p);
-  }
+  void getParams(Vector<FloatType> &into, int off);
 
   //For pipelining
   inline void resizeInputBuffer(size_t to){
@@ -397,3 +163,6 @@ auto skip_connection(Internal &&internal, Below &&below)-> LAYER_TYPE{
   return LAYER_TYPE(std::forward<Internal>(internal),std::forward<Below>(below));
 }
 #undef LAYER_TYPE
+
+
+#include "implementation/Layers.tcc"
