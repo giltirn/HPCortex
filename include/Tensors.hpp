84f499f296a6fab3903e5e715be10a732742f874
Christopher Kelly <ckelly@bnl.gov>
2025-03-31 16:31:31 -0400

    Completed implementation of Tensor with memory-managed memory, View, etc. + test For dimensions < 4, added accessors to Tensor that don't require passing the coordinate as an array Added dimension function to Vector and Matrix

--- include/Tensors.hpp
+++ include/Tensors.hpp
@@ -7,52 +7,139 @@
 #include <iostream>
 #include <ManagedArray.hpp>
 
-template<size_t dim>
-inline size_t tensorSize(const std::array<int,dim> &dims){
+template<size_t Dim>
+accelerator_inline size_t tensorSize(int const* dims){
   size_t out=1;
-  for(int d=0;d<dim;d++) out *= dims[d];
+#pragma unroll
+  for(int d=0;d<Dim;d++) out *= dims[d];
   return out;
 }
 template<size_t Dim>
-inline size_t compute_off(int const* coord, int const* dims){
+accelerator_inline size_t tensorOffset(int const* coord, int const* dims){
   size_t out = *coord++; ++dims;
+#pragma unroll
   for(int i=1;i<Dim;i++) out = out * (*dims++) + (*coord++);
   return out;
 }
+
 template<size_t Dim>
-inline size_t compute_off(const std::array<int,Dim> &coord, const std::array<int,Dim> &dims){
-  return compute_off<Dim>(coord.data(),dims.data());
+accelerator_inline void tensorOffsetUnmap(int * coord, int const* dims, size_t offset){
+  size_t rem = offset;
+#pragma unroll
+  for(int i=Dim-1;i>=0;i--){
+    coord[i] = rem % dims[i];
+    rem /= dims[i];
+  }
 }
+     
+ 
 
 template<typename _FloatType, int Dim>
 struct Tensor{
 public:
   typedef _FloatType FloatType;
 private:
-  std::vector<FloatType> vals;
+  ManagedArray<FloatType> vals;
   int _size[Dim];    
 public:
-  typedef std::array<int,Dim> Dims;
-  typedef std::array<int,Dim> Coord;
+  typedef int * Dims;
+  typedef int * Coord;
   
   constexpr int dimension(){ return Dim; }
   Tensor(): _size{0}{}
-  Tensor(const Dims &dims, FloatType init): vals(tensorSize(dims),init){ memcpy(_size,dims.data(),Dim*sizeof(int));  }
-  Tensor(const Dims &dims, const std::vector<FloatType> &init_vals): vals(init_vals){
-    memcpy(_size,dims.data(),Dim*sizeof(int));
-    assert(tensorSize(dims) == init_vals.size());
+  Tensor(const Dims dims, MemoryManager::Pool alloc_pool = MemoryManager::Pool::DevicePool): vals(tensorSize<Dim>(dims),alloc_pool){ memcpy(_size,dims,Dim*sizeof(int));  }
+  Tensor(const Dims dims, FloatType init, MemoryManager::Pool alloc_pool = MemoryManager::Pool::DevicePool): vals(tensorSize<Dim>(dims),init,alloc_pool){ memcpy(_size,dims,Dim*sizeof(int));  }
+  
+  Tensor(const Dims dims, const std::vector<FloatType> &init_vals): vals(init_vals){
+    memcpy(_size,dims,Dim*sizeof(int));
+    assert(tensorSize<Dim>(dims) == init_vals.size());
   }  
-  inline FloatType & operator()(const Coord &coord){
-    size_t off = compute_off<Dim>(coord.data(), _size);
-    return vals[off];
-  }
-  inline FloatType operator()(const Coord &coord) const{
-    size_t off = compute_off<Dim>(coord.data(), _size);
-    return vals[off];
-  }
 
   inline int size(int i) const{ return _size[i]; }
 
+  class View: private ManagedArray<FloatType>::View{
+    typedef typename ManagedArray<FloatType>::View Base;
+    int* _size;
+    bool is_device_ptr;
+  public:
+    inline View(ViewMode mode, const Tensor<FloatType,Dim> &parent): Base(mode, parent.vals){
+      if(mode == DeviceRead || mode == DeviceWrite || mode == DeviceReadWrite){
+	_size = (int*)acceleratorAllocDevice(Dim*sizeof(int));
+	acceleratorCopyToDevice(_size,parent._size,Dim*sizeof(int));
+	is_device_ptr = true;
+      }else{
+	_size = (int*)malloc(Dim*sizeof(int));
+	memcpy(_size,parent._size,Dim*sizeof(int));
+	is_device_ptr = false;
+      }	       
+    }
+
+    inline void free(){
+      if(is_device_ptr) acceleratorFreeDevice(_size);
+      else ::free(_size);      
+      return this->Base::free();
+    }
+    
+    accelerator_inline FloatType & operator()(const Coord coord){
+      return this->Base::operator[](tensorOffset<Dim>(coord, _size));
+    }
+    accelerator_inline FloatType operator()(const Coord coord) const{
+      return this->Base::operator[](tensorOffset<Dim>(coord, _size));
+    }
+
+    //1D tensor only
+    template<int D=Dim, typename std::enable_if<D==1,int>::type = 0>
+    accelerator_inline FloatType & operator()(int i){
+      return this->Base::operator[](i);
+    }
+    template<int D=Dim, typename std::enable_if<D==1,int>::type = 0>
+    accelerator_inline FloatType operator()(int i) const{
+      return this->Base::operator[](i);
+    }
+
+    //2D tensor only
+    template<int D=Dim, typename std::enable_if<D==2,int>::type = 0>
+    accelerator_inline FloatType & operator()(int i,int j){
+      return this->Base::operator[](j+_size[1]*i);
+    }
+    template<int D=Dim, typename std::enable_if<D==2,int>::type = 0>
+    accelerator_inline FloatType operator()(int i,int j) const{
+      return this->Base::operator[](j+_size[1]*i);
+    }
+
+    //3D tensor only
+    template<int D=Dim, typename std::enable_if<D==3,int>::type = 0>
+    accelerator_inline FloatType & operator()(int i,int j,int k){
+      return this->Base::operator[](k + _size[2]*(j+_size[1]*i));
+    }
+    template<int D=Dim, typename std::enable_if<D==3,int>::type = 0>
+    accelerator_inline FloatType operator()(int i,int j,int k) const{
+      return this->Base::operator[](k + _size[2]*(j+_size[1]*i));
+    }
+    
+    //4D tensor only
+    template<int D=Dim, typename std::enable_if<D==4,int>::type = 0>
+    accelerator_inline FloatType & operator()(int i,int j,int k,int l){
+      return this->Base::operator[](l+_size[3]*(k + _size[2]*(j+_size[1]*i)));
+    }
+    template<int D=Dim, typename std::enable_if<D==4,int>::type = 0>
+    accelerator_inline FloatType operator()(int i,int j,int k,int l) const{
+      return this->Base::operator[](l+_size[3]*(k + _size[2]*(j+_size[1]*i)));
+    }
+    
+    accelerator_inline FloatType const* data() const{ return this->Base::data(); }
+    accelerator_inline FloatType* data(){ return this->Base::data(); }
+    accelerator_inline size_t data_len() const{ return this->Base::size(); }
+
+    accelerator_inline size_t size(int i) const{ return _size[i]; }
+  };
+
+  View view(ViewMode mode) const{
+    return View(mode, *this);
+  }
+  //Lock the associated memory, preventing eviction
+  inline void lock() const{ vals.lock(); }
+  inline void unlock() const{ vals.unlock(); }  
 };
 
 template<typename _FloatType>
@@ -65,8 +152,9 @@ public:
   Vector(){}
   Vector(int size1, MemoryManager::Pool alloc_pool = MemoryManager::Pool::DevicePool): vals(size1,alloc_pool){}
   Vector(int size1, FloatType init, MemoryManager::Pool alloc_pool = MemoryManager::Pool::DevicePool): vals(size1, init, alloc_pool){}
-  Vector(const std::vector<FloatType> &init_vals): vals(init_vals){}    
-
+  Vector(const std::vector<FloatType> &init_vals): vals(init_vals){}
+  
+  constexpr int dimension(){ return 1; }
   inline size_t size(int i) const{ return vals.size(); }
 
   class View: private ManagedArray<FloatType>::View{
@@ -114,6 +202,7 @@ public:
     size0(size0), size1(size1), vals(size0*size1,init,alloc_pool){}
   Matrix(int size0, int size1, const std::vector<FloatType> &init_vals): size0(size0), size1(size1), vals(init_vals){}    
 
+  constexpr int dimension(){ return 2; }
   inline int size(int i) const{ return i==0 ? size0 : size1; }
   
   class View: private ManagedArray<FloatType>::View{
---
+++ test/test_tensor.cpp
@@ -0,0 +1,152 @@
+#include <HPCortex.hpp>
+#include <Testing.hpp>
+
+void testTensor(){
+  typedef double FloatType; 
+  typedef std::vector<FloatType> vecD;
+
+  //Test some basic functionality
+  {
+    int dims[3] = {2,3,4};
+    size_t size = 2*3*4;
+    assert( tensorSize<3>(dims) == size );
+            
+    vecD input(size);
+    vecD expect(size);
+    for(int i=0;i<2;i++)
+      for(int j=0;j<3;j++)
+	for(int k=0;k<4;k++){
+	  size_t off = k + 4*(j + 3*i);
+	  int coord[3] = {i,j,k};
+	  
+	  assert(tensorOffset<3>(coord,dims) == off);
+
+	  int test_coord[3];
+	  tensorOffsetUnmap<3>(test_coord,dims,off);
+	  for(int ii=0;ii<3;ii++) assert(test_coord[i] == coord[i]);	  
+	  
+	  input[off] = 3.141 * off;
+
+	  expect[off] = input[off] + 0.15*off*off;
+	}
+    
+    //Try a host-side kernel
+    Tensor<FloatType,3> tens_host(dims, input);
+    for(int d=0;d<3;d++)
+      assert(tens_host.size(d) == dims[d]);
+    
+    {
+      autoView(tens_host_v,tens_host,HostReadWrite);
+    
+      thread_for3d(i,2,j,3,k,4,{
+	  int coord[3] = {(int)i,(int)j,(int)k};
+	  size_t off = tensorOffset<3>(coord,dims);
+	  tens_host_v(coord) = tens_host_v(coord) + 0.15 * off * off;
+	});
+    }
+    {  
+      autoView(tens_host_v,tens_host,HostRead);
+      for(int i=0;i<2;i++)
+	for(int j=0;j<3;j++)
+	  for(int k=0;k<4;k++){
+	    size_t off = k + 4*(j + 3*i);
+	    int coord[3] = {(int)i,(int)j,(int)k};
+	    assert(abs_near(tens_host_v(coord), expect[off],1e-8 ));
+	  }
+    }
+
+    //Try a device-side kernel
+    Tensor<FloatType,3> tens_device(dims, input);
+    {
+      autoView(tens_device_v,tens_device,DeviceReadWrite);
+    
+      accelerator_for3d(i,2,j,3,k,4,  1,{
+	  int coord[3] = {(int)i,(int)j,(int)k};
+	  size_t off = tensorOffset<3>(coord,dims);
+	  tens_device_v(coord) = tens_device_v(coord) + 0.15 * off * off;
+	});
+    }
+    {  
+      autoView(tens_device_v,tens_device,HostRead);
+      for(int i=0;i<2;i++)
+	for(int j=0;j<3;j++)
+	  for(int k=0;k<4;k++){
+	    size_t off = k + 4*(j + 3*i);
+	    int coord[3] = {(int)i,(int)j,(int)k};
+	    assert(abs_near(tens_device_v(coord), expect[off], 1e-8) );
+	  }
+    }    
+    
+  }
+
+  //Test fixed dim accessors
+  {
+    {
+      vecD init({1.1,2.2,3.3});
+      int sz = 3;
+      Tensor<FloatType,1> tens_1(&sz, init);      
+      doHost(tens_1, { assert(tens_1_v(0) == 1.1 && tens_1_v(1) == 2.2 && tens_1_v(2) == 3.3); });
+    }
+    {
+      vecD init({
+	  1.1,2.2,3.3,
+	  4.4,5.5,6.6
+	});
+      int sz[2] = {2,3};
+      Tensor<FloatType,2> tens_2(sz, init);
+      doHost(tens_2, {
+	  for(int i=0;i<2;i++)
+	    for(int j=0;j<3;j++)
+	      assert(tens_2_v(i,j) == init[j+3*i]);
+	});
+    }
+
+    {
+      int sz[3] = {2,3,4};
+      vecD init(2*3*4);
+      for(int i=0;i<2;i++)
+	for(int j=0;j<3;j++)
+	  for(int k=0;k<4;k++)
+	    init[k+4*(j+3*i)] = (k+4*(j+3*i))*1.1;
+
+      Tensor<FloatType,3> tens_3(sz, init);
+      doHost(tens_3, {
+	  for(int i=0;i<2;i++)
+	    for(int j=0;j<3;j++)
+	      for(int k=0;k<4;k++)
+		assert(tens_3_v(i,j,k) == init[k+4*(j+3*i)]);
+	});
+    }
+
+    {
+      int sz[4] = {2,3,4,5};
+      vecD init(2*3*4*5);
+      for(int i=0;i<2;i++)
+	for(int j=0;j<3;j++)
+	  for(int k=0;k<4;k++)
+	    for(int l=0;l>5;l++)
+	      init[l+5*(k+4*(j+3*i))] = (l+5*(k+4*(j+3*i)))*1.1;
+
+      Tensor<FloatType,4> tens_4(sz, init);
+      doHost(tens_4, {
+	  for(int i=0;i<2;i++)
+	    for(int j=0;j<3;j++)
+	      for(int k=0;k<4;k++)
+		for(int l=0;l<5;l++)
+		  assert(tens_4_v(i,j,k,l) == init[l+5*(k+4*(j+3*i))]);
+	});
+    } 
+
+  }
+
+  
+  std::cout << "Test passed" << std::endl;
+}
+
+int main(int argc, char** argv){
+  initialize(argc,argv);
+  
+  testTensor();
+
+  return 0;
+}
