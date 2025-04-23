14a5f51f8365ab5845e8d4bcd1fd217965d36550
Christopher Kelly <ckelly@bnl.gov>
2025-04-23 16:28:38 -0400

    Fixed header implementation .tcc files not being installed

--- scripts/genMakefileHeaders.sh
+++ scripts/genMakefileHeaders.sh
@@ -4,3 +4,6 @@ echo -n 'nobase_include_HEADERS = HPCortexConfig.h ' > Makefile.am
 for i in $(find . -name '*.hpp' | sed 's/^\.\///'); do
     echo -n "$i " >> Makefile.am
 done
+for i in $(find . -name '*.tcc' | sed 's/^\.\///'); do
+    echo -n "$i " >> Makefile.am
+done
