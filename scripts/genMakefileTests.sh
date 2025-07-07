86e9938fbf4f24c3f39c31b74a398744984c91a1
Christopher Kelly <ckelly@bnl.gov>
2025-07-07 12:45:20 -0400

    Separated core and mpi tests into separate subdirectories

--- scripts/genMakefileTests.sh
+++ scripts/genMakefileTests.sh
@@ -1,22 +1,30 @@
 #Generate a Makefile for the test directory (run from inside the directory)
 
-out=Makefile.am
-echo 'SUBDIRS =' > ${out}
-echo 'AM_CPPFLAGS = -I$(top_srcdir)/include' >> ${out}
-echo 'AM_LDFLAGS = -Wl,-rpath=$(prefix)/lib' >> ${out}
-echo 'LDADD = -L$(top_builddir)/src -lmlcortex' >> ${out}
-echo 'testdir = $(prefix)/test' >> ${out}
+dirs=( core mpi )
+for dir in "${dirs[@]}"; do
+    echo $dir
+    cd ${dir}
+    
+    out=Makefile.am
+    echo 'SUBDIRS =' > ${out}
+    echo 'AM_CPPFLAGS = -I$(top_srcdir)/include' >> ${out}
+    echo 'AM_LDFLAGS = -Wl,-rpath=$(prefix)/lib' >> ${out}
+    echo 'LDADD = -L$(top_builddir)/src -lmlcortex' >> ${out}
+    echo "testdir = \$(prefix)/test/${dir}" >> ${out}
 
-echo -n 'test_PROGRAMS = ' >> ${out}
+    echo -n 'test_PROGRAMS = ' >> ${out}
 
-for i in $(find . -name '*.cpp' | sed 's/^\.\///' | sed 's/\.cpp//'); do
-    echo -n "$i " >> ${out}
+    for i in $(find . -name '*.cpp' | sed 's/^\.\///' | sed 's/\.cpp//'); do
+	echo -n "$i " >> ${out}
+    done
+    echo '' >> ${out}
+
+    for i in $(find . -name '*.cpp' | sed 's/^\.\///' | sed 's/\.cpp//'); do
+	echo "${i}_SOURCES = ${i}.cpp " >> ${out}
+	echo "${i}_LDADD = \$(LDADD)" >> ${out}
+	echo "${i}_LINK = \$(CXXLD) \$(AM_LDFLAGS) \$(LDFLAGS) -o \$@" >> ${out} #prevent autotools from including CXXFLAGS while linking!
+    done
+    echo '' >> ${out}
+
+    cd -
 done
-echo '' >> ${out}
-
-for i in $(find . -name '*.cpp' | sed 's/^\.\///' | sed 's/\.cpp//'); do
-    echo "${i}_SOURCES = ${i}.cpp " >> ${out}
-    echo "${i}_LDADD = \$(LDADD)" >> ${out}
-    echo "${i}_LINK = \$(CXXLD) \$(AM_LDFLAGS) \$(LDFLAGS) -o \$@" >> ${out} #prevent autotools from including CXXFLAGS while linking!
-done
-echo '' >> ${out}
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
 set -e
-rm -f run_tests.log
-./test_accelerator 2>&1 | tee test_accelerator.log
-./test_tensor 2>&1 | tee test_tensor.log
-./test_lossfunction 2>&1 | tee test_lossfunction.log
-./test_optimizer 2>&1 | tee test_optimizer.log
-./test_activation 2>&1 | tee test_activation.log
+
+cd core
+for i in $(find . -maxdepth 1 -type f -executable); do
+    ./${i} 2>&1 | tee "${i}.log"
+done
+cd -
+
+cd mpi
 mpirun -n  2 ./test_comms 2>&1 | tee test_comms.log
-./test_managed_array 2>&1 | tee test_managed_array.log
-./test_dynamic_model 2>&1 | tee test_dynamic_model.log
-./test_memorymanager 2>&1 | tee test_memorymanager.log
-./test_one_hidden_layer 2>&1 | tee test_one_hidden_layer.log
 mpirun -n 2 ./test_pipeline_1d 2>&1 | tee test_pipeline_1d.log
-./test_simple_linear 2>&1 | tee test_simple_linear.log
 mpirun -n 2 ./test_simple_linear_ddp 2>&1 | tee test_simple_linear_ddp.log
 mpirun -n 2 ./test_simple_linear_pipeline 2>&1 | tee test_simple_linear_pipeline.log
 mpirun -n 4 ./test_simple_linear_pipeline_ddp 2>&1 | tee test_simple_linear_pipeline_ddp.log
-./test_skip_connection 2>&1 | tee test_skip_connection.log
 mpirun -n 2 ./test_skip_connection_pipeline 2>&1 | tee test_skip_connection_pipeline.log
-./test_flatten_layer 2>&1 | tee test_flatten_layer.log
-./test_conv1d 2>&1 | tee test_conv1d.log
-./test_simple_linear_2d 2>&1 | tee test_simple_linear_2d.log
 mpirun -n 3 ./test_conv1d_pipeline 2>&1 | tee test_conv1d_pipeline.log
-./test_softmax 2>&1 | tee test_softmax.log
-./test_matrix_tensor_contract 2>&1 | tee test_matrix_tensor_contract.log
-./test_scaled_dotproduct_self_attention 2>&1 | tee test_scaled_dotproduct_self_attention.log
-./test_multihead_self_attention 2>&1 | tee test_multihead_self_attention.log
-./test_norm_layer 2>&1 | tee test_norm_layer.log
-./test_serialization 2>&1 | tee test_serialization.log
